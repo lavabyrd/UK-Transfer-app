fb9c5f5f37dc78be1aa8944195466aeb617265bc
splinterific <markgpreston@gmail.com>
2014-09-19 11:44:14 +0100

    added timeout
    requirement for script to run every 30 seconds as Chron will only occur every 1 minute

--- initiate.bat
+++ initiate.bat
@@ -2,3 +2,7 @@
 MOVE/y c:\new\* c:\import
 START /WAIT c:\DHL\import.exe \import 33 \output
 DEL /Q c:\import\*
+TIMEOUT /t 30
+MOVE/y c:\new\* c:\import
+START /WAIT c:\DHL\import.exe \import 33 \output
+DEL /Q c:\import\*
